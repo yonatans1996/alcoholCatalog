7fa91bbd262ab6d8cdb7469753e46834eda5a201
yonatans1996 <yonatan.shtalhaim@gmail.com>
2021-02-23 01:05:37 +0200

    css fix

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -43,14 +43,8 @@ function Login() {
                         </Form.Group>
                         <Button disabled={loading} className="w-100" type="submit">Log In</Button>
                     </Form>
-                    <div className="w-100 text-center mt-3">
-                    <Link to="/forgot-password">Forgot Password?</Link>
-                    </div>
                 </Card.Body>
             </Card>
-            <div className="w-100 text-center mt-2">
-                    Need an account? <Link to="/signup">Sign Up</Link>
-                </div>
         </div>
     )
 }
