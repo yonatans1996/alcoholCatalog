ab456857ad3e16ba816ac6b65bc82b75b52a827b
yonatans1996 <yonatan.shtalhaim@gmail.com>
2021-02-23 00:45:33 +0200

    added user authentication /login /admin

--- src/App.js
+++ src/App.js
@@ -5,17 +5,31 @@ import Info from "./components/Info"
 import Products from "./components/Products"
 import Form from "./components/Form"
 import Special from "./components/Special"
+import { AuthProvider } from './components/Auth/AuthContext'
+import PrivateRoute from "./components/Auth/PrivateRoute"
+import Login from "./components/Auth/Login"
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="site">
-      <Header/>
-      <Info/>
-      <Products location={true} />
-      <Special/>
-      <Products location={false} />
-      <Form/>
+      <Router>
+        <AuthProvider>
+          <Switch>
+          <PrivateRoute exact path="/admin" component={Form}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/">
+                <Header/>
+                <Info/>
+                <Products location={true} />
+                <Special/>
+                <Products location={false} />
+            </Route>
+          </Switch>
+        </AuthProvider>
+      </Router>
+
     </div>
   );
 }
